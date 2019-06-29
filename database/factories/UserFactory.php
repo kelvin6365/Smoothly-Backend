99d79dc5247f0024e42ea6564edc389d1fb9809d
kelvin6365 <kelvin6365@gmail.com>
2019-06-30 04:47:21 +0800

    user table done, user seeder done, user graphql done

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,9 +19,12 @@ use Faker\Generator as Faker;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
+        'username' => $faker->userName,
         'email' => $faker->unique()->safeEmail,
+        'permission'=>$faker->randomElement($array = array ('1','1,2','2','2,3','3')),
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => Hash::make('123456'), // password
+        'user_type' =>$faker->randomElement($array = array ('1','2')),
         'remember_token' => Str::random(10),
     ];
 });
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('permission');
+            $table->string('user_type',1);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
